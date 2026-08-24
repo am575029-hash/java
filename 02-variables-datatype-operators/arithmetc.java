d37187cde7aec4c8d794df3df5b12e69d5296915
am575029-hash <am575029@gmail.com>
2026-08-24 22:10:59 +0530

    control-statements and loops

--- 02-variables-datatype-operators/arithmetc.java
+++ 02-variables-datatype-operators/arithmetc.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class add_two_number {
+public class arithmetc {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
          System.out.println("enter a:");
@@ -8,8 +8,18 @@ public class add_two_number {
         
         System.out.println("enter b:");
         int b= sc.nextInt();
+
         int sum=a+b;
         System.out.println("sum= "+sum);
+         int sub=a-b;
+        System.out.println("sub= "+sub);
+         int multiply=a+b;
+        System.out.println("multiply= "+multiply);
+         int divide=a/b;
+        System.out.println("divide= "+divide);
+         int remendar=a%b;
+        System.out.println("remendar= "+remendar);
+
         sc.close();
     }
 }
---
+++ 03-control-statement/switchcase.java
@@ -0,0 +1,19 @@
+
+
+public class switchcase {
+    public static void main(String[] ages){
+     int  num=2;
+     switch(num){
+        case 1:
+            System.out.println("Numbar is 1");
+            break;
+        case 2:
+            System.out.println("Numbar is 2");
+            break;  
+         default:
+            System.out.println("Numbar is not here");
+           
+     }
+
+    }
+}
